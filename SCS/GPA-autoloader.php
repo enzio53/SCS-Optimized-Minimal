fa8f297eadf62246d5c2893a4cf5e048737505f7
enzio53 <codemaxgames@gmail.com>
2018-02-13 23:37:44 +0100

    First version

---
+++ SCS/GPA-autoloader.php
@@ -0,0 +1,17 @@
+<?php
+$GPA_PLUGINS['name']        = 'SCS';
+$GPA_PLUGINS['role']        = 'classes';
+$GPA_PLUGINS['location']    = 'functions/';
+$GPA_PLUGINS['version']     = '1.0';
+$GPA_PLUGINS['status']      = 'enabled';
+
+$GPA_PLUGINS['condenser']   = 'disabled';
+
+/**
+ *
+ * SCS-Optimized-Minimal (work with GPA)
+ * @author Enzo Poker <enzio@garryhost.com>
+ * @link https://github.com/enzio53/GPA
+ * @version 1.0
+ *
+ */
